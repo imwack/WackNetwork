dc8ce61979e0d74192970ba25b86dfd0b7c719ef
imwack <i.am.wack.1993@gmail.com>
2019-03-20 22:03:00 +0800

    add servernetwork code

--- Server/Server/Connection.cs
+++ Server/Server/Connection.cs
@@ -1,21 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Server.Util;
 
 namespace Server
 {
-    public class Connection
+    public interface IRemote
+    {
+        string RemoteIp { get; }
+        int RemotePort { get; }
+        int Id { get; }
+
+        int Push(byte[] buffer, int len, int offset);
+        int PushBegin(int len);
+        int PushMore(byte[] buffer, int len, int offset);
+    }
+
+    public interface ILocal
+    {
+        string RemoteIp { get; }
+        int RemotePort { get; }
+    }
+
+    internal class Connection : IRemote, ILocal
     {
         private Socket clientSocket;
-        private int connectionId;
+        public int connectionId;
+
+
+        internal bool Connected { get; set; }
+
+        public string RemoteIp
+        {
+            get
+            {
+                if (clientSocket != null)
+                {
+                    var ipEndPoint = clientSocket.RemoteEndPoint as IPEndPoint;
+                    if (ipEndPoint != null)
+                    {
+                        return ipEndPoint.Address.ToString();
+                    }
+                }
+                return "";
+            }
+        }
+
+        public int RemotePort
+        {
+            get
+            {
+                if (clientSocket != null)
+                {
+                    var ipEndPoint = clientSocket.RemoteEndPoint as IPEndPoint;
+                    if (ipEndPoint != null)
+                    {
+                        return ipEndPoint.Port;
+                    }
+                }
+                return 0;
+            }
+        }
+
+        public bool DefferedClose { get; set; }
+
+        private ConnectionBuffer receiveBuffer = new ConnectionBuffer();
+        private ConnectionBuffer sendBuffer = new ConnectionBuffer();
+
+        private readonly SwapContainer<Queue<Message>> messageQueue = new SwapContainer<Queue<Message>>();
+
 
         public Connection(Socket clientSocket, int id)
         {
             this.clientSocket = clientSocket;
             this.connectionId = id;
+            Connected = true;
+        }
+
+        public void ProcessMessageQueue(Action<IRemote , Message> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Push(byte[] buffer, int len, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PushBegin(int len)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PushMore(byte[] buffer, int len, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginReceive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Server/Server/ServerNetwork.cs
+++ Server/Server/ServerNetwork.cs
@@ -15,12 +15,20 @@ namespace Server
         private int currentConnectionId = 1;
         private int minConnectionId = 1;
         private int maxConnectionId = int.MaxValue;
+        private Socket listenSocket;
 
         private readonly Dictionary<int, Connection> clientDict = new Dictionary<int, Connection>();
-        private readonly SwapContainer<Queue<Connection>> waitClient = new SwapContainer<Queue<Connection>>();
-        private readonly SwapContainer<Queue<Connection>> closeClient = new SwapContainer<Queue<Connection>>();
+        private readonly SwapContainer<Queue<Connection>> waitClientQueue = new SwapContainer<Queue<Connection>>();
+        private readonly SwapContainer<Queue<Connection>> closeClientQueue = new SwapContainer<Queue<Connection>>();
 
-        private Socket listenSocket;
+        public delegate void ServerNetworkClientConnectedHandler(IRemote remote);
+        public delegate void ServerNetworkClientDisconnectedHandler(IRemote remote);
+        public delegate void ServerNetworkClientMessageReceivedHandler(IRemote remote, Message msg);
+
+        public ServerNetworkClientConnectedHandler OnClientConnected;
+        public ServerNetworkClientDisconnectedHandler OnClientDisconnected;
+        public ServerNetworkClientMessageReceivedHandler OnReceiveClientMessage;
+
 
         public ServerNetwork(int port)
         {
@@ -47,9 +55,9 @@ namespace Server
                 if (id != -1)
                 {
                     var clientConnection = new Connection(clientSocket, id);
-                    lock (waitClient.Lock)
+                    lock (waitClientQueue.Lock)
                     {
-                        waitClient.In.Enqueue(clientConnection);
+                        waitClientQueue.In.Enqueue(clientConnection);
                     }
                     Logger.Info("OnAcceptCallback add wait client id={0}", id);
                 }
@@ -82,11 +90,107 @@ namespace Server
         }
         public void OneLoop()
         {
-            
+            try
+            {
+                // todo heartbeat
+
+                ProcessClientConnectMessageQueue();
+                RefreshClientList();
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
         }
 
         public void Close()
         {
+            listenSocket?.Close();
+            Logger.Debug("ServerSocket.Dispose");
+        }
+
+        private void ProcessClientConnectMessageQueue()
+        {
+            foreach (var clientConnection in clientDict.Values)
+            {
+                clientConnection.ProcessMessageQueue((c, msg) =>
+                {
+                    OnReceiveClientMessage?.Invoke(c, msg);
+                });
+            }
+        }
+
+        private void RefreshClientList()
+        {
+            // accept client connection
+            if (waitClientQueue.Out.Count == 0)
+            {
+                waitClientQueue.Swap();
+
+                foreach (var clientConnection in waitClientQueue.Out)
+                {
+                    // todo race condition, when add conn to clientConnectorsDict
+                    if (clientDict.ContainsKey(clientConnection.connectionId))
+                    {
+                        Logger.Warn("ServerNetwork.RefreshClientList connector exist id={0}", clientConnection.connectionId);
+                        return;
+                    }
+
+                    clientDict.Add(clientConnection.connectionId, clientConnection);
+                    OnClientConnected?.Invoke(clientConnection);
+
+                    // client.sysSocket.BeginReceive
+                    clientConnection.BeginReceive();
+                }
+                waitClientQueue.Out.Clear();
+            }
+
+            // remove client connection
+            if (closeClientQueue.Out.Count == 0)
+            {
+                closeClientQueue.Swap();
+                foreach (var clientConnection in closeClientQueue.Out)
+                {
+                    if (clientDict.ContainsKey(clientConnection.connectionId))
+                    {
+                        clientDict.Remove(clientConnection.connectionId);
+                        clientConnection.Close();
+                        OnClientDisconnected?.Invoke(clientConnection);
+                    }
+                }
+                closeClientQueue.Out.Clear();
+            }
+
+            foreach (var client in clientDict.Values)
+            {
+                client.Send();
+                if (client.DefferedClose)
+                {
+                    // close any clients which failed sending data
+                    CloseClient(client, NetworkCloseMode.DefferedClose);
+                }
+            }
+        }
+
+        private void CloseClient(Connection connection, NetworkCloseMode mode)
+        {
+            if (connection == null)
+            {
+                Logger.Warn("ServerNetwork.CloseClient socket is null");
+                return;
+            }
+            lock (closeClientQueue.Lock)
+            {
+                closeClientQueue.In.Enqueue(connection);
+                Logger.Debug("ServerNetwork.CloseClient connectId={0}", mode, connection.connectionId);
+            }
+        }
+
+        public enum NetworkCloseMode
+        {
+            HeartbeatTimeout = 1,
+            DefferedClose = 2,
+            Dispose = 3,
         }
     }
 }
